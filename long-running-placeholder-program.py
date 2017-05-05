df38334ed757938860a0a2d4613cdb1f4bade98f
gisdev01 <gisdev01@gmail.com>
2017-05-04 22:11:20 -0400

    Improve long-running process testing for round-robin assigments to consumers for RabbitMQ

--- long-running-placeholder-program.py
+++ long-running-placeholder-program.py
@@ -5,6 +5,12 @@ import time
 import pika
 import json
 
+import logging
+
+LOG_FORMAT = '%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
+
 rabbit_hostname = 'localhost'
 rabbit_queue_name = 'ids_queue'
 
@@ -20,14 +26,22 @@ rabbit_channel.queue_declare(queue=rabbit_queue_name, durable=True)
 
 # This fires as a callback every time we receive a new message on the queue
 def rabbit_msg_receiver(ch, method, properties, body):
-    print("Received message body: {}".format(body))
+    logger.info("Received message body: {}".format(body))
     # Need to decode the bytes body into a string, so we can load into a dict from a raw json string
     msg_dict = json.loads(body.decode('utf-8'))
     ids_to_send_to_program = msg_dict['ids']
-    print(ids_to_send_to_program)
-    sleep_seconds_calc = 10
-    time.sleep(sleep_seconds_calc)
+    logger.info("Sending to long-running process: {}".format(ids_to_send_to_program))
+
+    long_running_processing(ids_to_send_to_program)
     ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def long_running_processing(ids_to_process):
+    sleep_seconds_calc = 1000
+    logger.info('IDs sent to long running process: {} '.format(ids_to_process))
+
+    #Block the long-running function for a really long time and make sure no messages are ingested while this function is running
+    time.sleep(sleep_seconds_calc)
 
 
 # Basic QOS which will only send messages to consumers after they have ack'ed their previous message. This keeps the load even throughout the consumers.
--- rabbit-sender.py
+++ rabbit-sender.py
@@ -1,14 +1,12 @@
 # Rabbit MQ Testing in Python 3.5 - as a work queue
 # Rabbit assumed to be running on localhost:5672
 
-import pika
-import sys
 import json
+import sys
+
+import pika
 
 test_msg = ' '.join(sys.argv[1:])
-
-ids_to_send = ['1122', '2222']
-sample_msg = {'ids': ids_to_send}
 
 # Connecting to the localhost RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -22,15 +20,17 @@ channel.queue_declare(queue=rabbit_queue_name, durable=True)
 # can list this queue in a shell with: sudo rabbitmqctl list_queues
 
 
-channel.basic_publish(exchange='',
-                      routing_key=rabbit_queue_name,
-                      body=json.dumps(sample_msg),
-                      properties=pika.BasicProperties(
-                          # Make this message persistent
-                          delivery_mode=2,
-                      ))
+for i in range(100):
+    ids_to_send = [i, i + 1000, i + 10000]
+    sample_msg = {'ids': ids_to_send}
+    channel.basic_publish(exchange='',
+                          routing_key=rabbit_queue_name,
+                          body=json.dumps(sample_msg),
+                          properties=pika.BasicProperties(
+                              # Make this message persistent
+                              delivery_mode=2,
+                          ))
 
 print("Sent Message: {}".format(sample_msg))
-
 
 connection.close()
